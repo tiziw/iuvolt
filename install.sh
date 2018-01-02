e0088cb8bced85824130b64e33f17a158135008f
tiziw <34965470+tiziw@users.noreply.github.com>
2018-01-02 22:41:56 +0100

    Improve systemd service
    - Systemd service now also runs after suspend
    - Script at /usr/lib/systemd/system-sleep no longer needed

--- install.sh
+++ install.sh
@@ -8,8 +8,4 @@ cp iuvolt /usr/bin/
 chmod +x /usr/bin/iuvolt
 cp systemd-iuvolt.service /etc/systemd/system/
 systemctl enable iuvolt.service
-echo '
-#!/bin/bash
-iuvolt' >> /usr/lib/systemd/system-sleep/iuvolt
-chmod +x /usr/lib/systemd/system-sleep/iuvolt
 echo "voltages=(0 0 0)" >> /etc/iuvolt.cfg
