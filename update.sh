0ec1337d1d2506a7f74e162e2e757a7d191352b9
Kilian Gosewisch <kilian2798@gmail.com>
2018-04-17 15:47:24 +0200

    checked for old service file

--- update.sh
+++ update.sh
@@ -6,6 +6,20 @@ if [ "$(id -u)" != "0" ]; then
 fi
 cp iuvolt /usr/bin/
 chmod +x /usr/bin/iuvolt
+status=disabled
+
+if [ -f /etc/systemd/system/systemd-iuvolt.service ]; then
+	status=$(systemctl is-enabled systemd-iuvolt.service)
+	systemctl stop systemd-iuvolt.service
+	systemctl disable systemd-iuvolt.service
+	rm /etc/systemd/system/systemd-iuvolt.service
+fi
+
 cp iuvolt.service /etc/systemd/system/
 systemctl daemon-reload
+
+if [ "$status" = "enabled" ]; then
+	systemctl enable iuvolt.service
+fi
+
 systemctl restart iuvolt.service
